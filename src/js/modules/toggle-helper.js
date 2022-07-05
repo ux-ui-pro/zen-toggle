196c9374cdf018e9f5f1cfc9cd4101b14490bc9c
ux-ui-pro <51360899+ux-ui-pro@users.noreply.github.com>
2022-07-06 00:34:37 +0300

    issue: updated the ripple effect

--- src/js/modules/toggle-helper.js
+++ src/js/modules/toggle-helper.js
@@ -18,7 +18,7 @@ export default function toggleHelper() {
 
                 setTimeout(() => {
                     el.classList.remove('toggle--ripple')
-                }, 225)
+                }, 325)
             })
 
         })
